80d5830bdc6101bca95a8f6d43d0c463cc3b167b
Azeskiv <acasadojareno@gmail.com>
2023-11-15 12:49:16 +0100

    Controlador configProperties

--- T2_AlvaroGuzmanAlfonso/src/Controller/Controlador.java
+++ T2_AlvaroGuzmanAlfonso/src/Controller/Controlador.java
@@ -3,10 +3,53 @@ package Controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
-
 import View.Vista;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.Set;
+import Model.Ciudad;
 
 public class Controlador implements ActionListener {
+	
+	
+	 private List<Ciudad> ciudades;
+
+	    public Controlador() {
+	        cargarCiudadesDesdeConfig();
+	    }
+
+	    private void cargarCiudadesDesdeConfig() {
+	        ciudades = new ArrayList<>();
+
+	        Properties configuracion = new Properties();
+
+	        try {
+	            configuracion.load(new FileReader("src/resources/config.properties"));
+	            Set<String> nombresCiudades = configuracion.stringPropertyNames();
+
+	            for (String nombreCiudad : nombresCiudades) {
+	                String urlJson = configuracion.getProperty(nombreCiudad);
+	                Ciudad ciudad = new Ciudad(nombreCiudad, urlJson);
+	                ciudades.add(ciudad);
+	            }
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+	    public List<Ciudad> getCiudades() {
+	        return ciudades;
+	    }
+
+	    // Aquí puedes implementar lógica para recuperar y analizar información climatológica desde las URLs JSON.
+	    // Puedes usar Gson u otra biblioteca para trabajar con JSON.
+	    // También podrías tener métodos para obtener el pronóstico de una ciudad específica, etc.
+	    // ...
+
+	
 	ArrayList<String> Andalucia = new ArrayList<>();
 	ArrayList<String> Baleares = new ArrayList<>();
 	ArrayList<String> Canarias = new ArrayList<>();
@@ -36,5 +79,4 @@ public class Controlador implements ActionListener {
 		
 		
 	}
-	
 }
---
+++ T2_AlvaroGuzmanAlfonso/src/Model/Ciudad.java
@@ -0,0 +1,35 @@
+package Model;
+public class Ciudad {
+    private String nombre;
+    private String urlJson;
+
+    public Ciudad(String nombre, String urlJson) {
+        this.nombre = nombre;
+        this.urlJson = urlJson;
+    }
+
+    // Getters y setters (puedes generarlos automáticamente en Eclipse)
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUrlJson() {
+        return urlJson;
+    }
+
+    public void setUrlJson(String urlJson) {
+        this.urlJson = urlJson;
+    }
+
+    @Override
+    public String toString() {
+        return "Ciudad{" +
+                "nombre='" + nombre + '\'' +
+                ", urlJson='" + urlJson + '\'' +
+                '}';
+    }
+}
